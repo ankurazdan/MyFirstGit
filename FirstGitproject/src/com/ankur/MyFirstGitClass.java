367cc7ed7a2a667179bbd689db50659b64bb0cb9
ankurazdan <47125323+ankurazdan@users.noreply.github.com>
2019-01-29 11:45:08 -0600

    commit 11:44 direct on github

--- FirstGitproject/src/com/ankur/MyFirstGitClass.java
+++ FirstGitproject/src/com/ankur/MyFirstGitClass.java
@@ -3,7 +3,7 @@ package com.ankur;
 public class MyFirstGitClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World12345");
+		System.out.println("Hello World123456");
 		
 		System.out.println("Hello World2");
 
